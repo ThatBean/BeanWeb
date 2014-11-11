a575173ac822194ecd5c878b1b6db0318023b8a0
ThatBean <Goomailbean@gmail.com>
2014-11-11 08:15:09 +0800

    adding font

--- js_canvas/src/scripts/BeanPixel.image.js
+++ js_canvas/src/scripts/BeanPixel.image.js
@@ -15,9 +15,8 @@ var image = (function () {
 		//console.log(document.body);
 		//console.log(document.getElementsByTagName('body')[0]);
 		//console.log(document.getElementById('BeanMainCanvas'));
-		var font_canvas = B_Toolbox.createElement(document.body, 'canvas', 'B_font_canvas');
-		//font_canvas.style = 'display:none;';
-		font_canvas.style.display = '';
+		var font_canvas = document.getElementById('B_font_canvas') || B_Toolbox.createElement(document.body, 'canvas', 'B_font_canvas');
+		font_canvas.style.display = 'none';
 		font_canvas.style.position = 'fixed';
 		font_canvas.style.top = '200px';
 		font_canvas.style.right = '0px';
@@ -36,7 +35,7 @@ var image = (function () {
 		var font_ctx = font_canvas.getContext('2d');
 		
 		
-		var res_font_obj = {};
+		image.res_font_obj = {};
 		font_img.onload = function () {
 			font_ctx.drawImage(font_img,0,0);
 		
@@ -45,17 +44,51 @@ var image = (function () {
 			
 			for (var i in font_map) {
 				var image_data = font_ctx.getImageData(i * font_width, 0, font_width, font_height);
-				res_font_obj[font_map[i]] = image_data;
+				image.res_font_obj[font_map[i]] = image_data;
 				console.log(i, "get", font_map[i], i * font_width, 0, font_width, font_height)
 				
 			}
 		
 		}
-		console.log(res_font_obj);
+		console.log(image.res_font_obj);
 		
 		//image.test_idata =  font_ctx.getImageData(0, 0, font_width * font_map.length, font_height);
 		
-		return res_font_obj;
+		return image.res_font_obj;
+	}
+	
+	image.getFontString= function (tgt_string, line_width) {
+		image.loadFont();
+		
+		var font_canvas = document.getElementById('B_font_canvas');
+		var font_ctx = font_canvas.getContext('2d');
+		
+		//image.res_font_obj;
+		
+		var str_length = tgt_string.length;
+		var data_width = str_length < line_width ? str_length : line_width;
+		var data_height = Math.floor(str_length / line_width + 1);
+		
+		var image_data = font_ctx.createImageData(data_width * font_width, data_height * font_height);
+		
+		for (var i in tgt_string) {
+			
+			
+			
+			var image_data = font_ctx.getImageData(i * font_width, 0, font_width, font_height);
+			
+			
+			
+			image.res_font_obj[font_map[i]] = image_data;
+			console.log(i, "get", font_map[i], i * font_width, 0, font_width, font_height)
+			
+		}
+		
+		console.log(image.res_font_obj);
+		
+		//image.test_idata =  font_ctx.getImageData(0, 0, font_width * font_map.length, font_height);
+		
+		return image.res_font_obj;
 	}
 	
 	return image;
