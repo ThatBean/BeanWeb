3111a6aad4f8e8a690431d8efbec2a605614ed9f
ThatBean <Goomailbean@gmail.com>
2015-02-08 23:48:20 +0800

    add module

--- Dr.js
+++ Dr.js
@@ -84,6 +84,8 @@ var Dr = (typeof Dr == "function" && Dr.author == DrAuthor && Dr.verion >= DrVer
 	Dr.author = DrAuthor;
 	Dr.verion = DrVersion;
 	
+	Dr.global = window;
+	
 	Dr.objectConstructor = {
 		Array: Array,
 		Function: Function,
@@ -107,32 +109,111 @@ var Dr = (typeof Dr == "function" && Dr.author == DrAuthor && Dr.verion >= DrVer
 	
 	Dr.now = dr_required_native.getUTCTimeStamp;
 	Dr.startTimestamp = Dr.now();
-	Dr.clock = function () { return (Dr.now() - Dr.startTimestamp); }
+	Dr.clock = function () { return (Dr.now() - Dr.startTimestamp); };
 	
-	
-	/*
-		Dr Module can be a function or an object
+	Dr.module_manager = (function () {
+		/*
+			Dr Module can be a function or an object
+			module_type can be: function, object, ...
+		*/
+		var module_manager = {};
 		
-		module_type can be: function, object, ...
-	*/
-	
-	Dr.declare =  function (module_name, module_type) {}
-	Dr.implement =  function (module_name, module_implement_func) {}
-	Dr.require =  function (module_name, required_module_name) {}
-	
-	Dr.module_pool = {}
-	Dr.module_get = function (module_name) { return Dr.module_pool[module_name]; }
-	Dr.module_set = function (module_name, module_instance) { Dr.module_pool[module_name] = module_instance; }
-	
-	/*
-	//this will be called when all required module implementation is get
-	module_implement_func = function (module_global_object, required_module_get_func) {
-		var module_play = required_module_get_func("module_play");
-		var module_stop = required_module_get_func("module_stop");
+		module_manager._module_pool = {};
 		
-		//implementation
-	}
-	*/
+		module_manager._module_init = function (module_name) { 
+			this._module_pool[module_name] = {
+				status: "declared",
+				require: [],
+				module: null,
+				implement_func: null,
+				type: module_type,
+				name: module_name,
+			};
+		};
+		module_manager._module_get = function (module_name) { 
+			if (this._module_pool[module_name] && this._module_pool[module_name].status == "loaded")
+				return this._module_pool[module_name].module;
+			else
+				return null;
+				//alert("get module failed! name: " + module_name);
+		};
+		module_manager._module_set = function (module_name, module) {
+			if (!this._module_pool[module_name]) {
+				alert("module not declared");
+				return;
+			}
+			this._module_pool[module_name].module = module; 
+			this._module_pool[module_name].status = "loaded";
+		};
+		
+		module_manager.declare =  function (module_name, module_type) {
+			if (this._module_pool[module_name] && module_type != this._module_pool[module_name].type) {
+				alert("re-declare type mismatch");
+				return;
+			};
+			
+			this._module_init(module_name);
+		};
+		module_manager.implement =  function (module_name, module_implement_func) {
+			if (!this._module_pool[module_name]) {
+				alert("module not declared");
+				return;
+			}
+			
+			this._module_pool[module_name].implement_func = module_implement_func;
+			this._module_pool[module_name].status = "implemented";
+		};
+		module_manager.require =  function (module_name, required_module_name) {
+			if (!this._module_pool[module_name]) {
+				alert("module not declared");
+				return;
+			}
+			
+			this._module_pool[module_name].require.push(required_module_name);
+			this._module_pool[module_name].status = "required";
+		};
+		module_manager.load =  function (module_name) {
+			if (!this._module_pool[module_name]) {
+				alert("module not declared");
+				return;
+			}
+			if (this._module_pool[module_name].status == "loaded") {
+				return;
+			}
+			
+			//loop for all required
+			var require_name_list = this._module_pool[module_name].require;
+			var all_required_implemented = true;
+			for (var i in require_name_list) {
+				if (!this._module_get(require_name_list[i])) {
+					all_required_implemented = false;
+				}
+			}
+			
+			if (all_required_implemented) {
+				var module = this._module_pool[module_name].implement_func(this.global, this._module_get);
+				this._module_set(module_name, module);
+			}
+			else
+				return;
+		};
+		
+		/*
+		//this will be called when all required module implementation is get
+		
+		module_implement_func = function (global, required__module_get_func) {
+			var module_play = required__module_get_func("module_play");
+			var module_stop = required__module_get_func("module_stop");
+			
+			//global usually means window
+			
+			//implementation
+		}
+		*/
+		
+		return module_manager;
+	})()
+	
 	Dr.Time = (function () {
 		var Time = function () {
 			
