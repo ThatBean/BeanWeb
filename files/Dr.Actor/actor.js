d7bdcfcfe15871e7b71a76776f75f1773c8beb7d
ThatBean <Goomailbean@gmail.com>
2015-04-02 10:02:04 +0800

    minor change

--- files/Dr.Actor/actor.js
+++ files/Dr.Actor/actor.js
@@ -1,12 +1,14 @@
 /** Actor 
  **** Dependency
- * Needs nothing
+ * ActorStage
+ * ActorData
+ * ActorStatePool
+ * -- ActorState
+ * ActorActionPool
+ * -- ActorAction
  * 
  **** Content
  * Actor
- * Data
- * Logic
- * Action
  * 
  * 
  * 
@@ -19,33 +21,51 @@ Dr.Implement('Actor', function (global, module_get) {
 	
 	var Actor = (function () {
 		function Actor() {
-			this.r = r;
-			this.g = g;
-			this.b = b;
-			this.a = a;
+			this._status = 'un_inited';
 		}
 		
 		Actor.prototype.init = function () {
-			this._actor_id = null;	//used mostly with stage
+			this._id = null;	//used mostly with stage
 			
 			this._is_active = false;
 			this._is_pause = false;
 			
-			this._actor_stage = null;
-			this._actor_data = new ActorData(this);
-			this._actor_control = new ActorControl(this);
-			this._actor_state = new ActorState(this);
-			this._actor_action = new ActorAction(this);
+			this._stage = null;	//the outer environment for this actor(or ActorManager, sort of)
+			
+			this._data = new ActorData(this);		//data stored by this actor
+			this._control = new ActorControl(this);	//superior to basic state flow, process manual/auto/scripted-auto control
+			this._state_pool = new ActorStatePool(this);	//There are several kind of states for actor(idle, dead, move, attack, etc.), there can be more than 1 active state in pool
+			this._action_pool = new ActorActionPool(this);	//There are several kind of actions for actor
 		};
 		
-		Actor.prototype.getActorData = function () { return this._actor_data; };
-		Actor.prototype.getActorId = function () { return this._actor_id; };
 		
-		Actor.prototype.setActorStage = function (actor_stage) { this._actor_stage = actor_stage; };
-		Actor.prototype.getActorStage = function () { return this._actor_stage; };
+		Actor.prototype.setId = function (id) { this._id = _id; };
+		Actor.prototype.getId = function () { return this._id; };
+		
+		Actor.prototype.setStage = function (stage) { this._stage = stage; };
+		Actor.prototype.getStage = function () { return this._stage; };
+		
+		Actor.prototype.getData = function () { return this._data; };
+		//private
+		//Actor.prototype.getControl = function () { return this._control; };
+		//Actor.prototype.getStatePool = function () { return this._state_pool; };
+		//Actor.prototype.getAction = function () { return this._action; };
+		
 		
 		Actor.prototype.update = function (delta_time) {
 			//
+			if (this._is_pause === false && this._is_active === true) {
+				//update
+				
+				this._data.update(delta_time);
+				this._control.update(delta_time);
+				this._state_pool.update(delta_time);
+				this._action_pool.update(delta_time);
+				
+				
+			}
+			
+			
 		};
 		
 		
