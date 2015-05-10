7aee4e86ccf19590347fd775524f86ed040de4df
ThatBean <Goomailbean@gmail.com>
2015-05-10 16:54:40 +0800

    sorting failed...

--- files/Dr.Image/image.js
+++ files/Dr.Image/image.js
@@ -13,45 +13,56 @@ Dr.Implement('ImageData', function (global, module_get) {
 	}
 	
 	Module.prototype.init = function (source, data, type) {
-		this._source = source;
 		
 		switch (type) {
 			case Module.type.IMAGE_ELEMENT:
+				this._editable = false;
+				this.draw = this.drawImage;
+				this.drawClip = this.drawImageClip;
+				break;
 			case Module.type.CANVAS_ELEMENT:
+				this._editable = true;
+				this.draw = this.drawImage;
+				this.drawClip = this.drawImageClip;
+				break;
 			case Module.type.CANVAS_IMAGE_DATA:
-				this._width = data.width;
-				this._height = data.height;
+				this._editable = true;
+				this.draw = this.drawImageData;
+				this.drawClip = this.drawImageDataClip;
 				break;
 			default:
 				Dr.log('[ImageData] error type:', type, source);
 				break;
 		}
 		
-		this._type = type;
+		//all directly accessible(public)
+		this.type = type;
+		this.data = this._editable ? data : null;
+		
+		this.width = data.width;
+		this.height = data.height;
+		
+		//should not access(private)
+		this._source = source;
 		this._data = data;
 	}
 	
-	Module.prototype.load = function (font_data) {
-		if (
-			!font_data.font_src
-			|| !font_data.font_map
-			|| !(
-				(font_data.font_width && font_data.font_height) || font_data.font_size
-			)
-		) {
-			Dr.log("[loadFontData] error font_data", font_data);
-			return;
-		}
-		
-		this.font_data = font_data;
-		this.font_map = font_data.font_map;
-		this.font_map = font_data.font_map;
-		
-		
-		Dr.loadImage(font_data.font_src, function (image_element) {
-			Dr.log("[loadFontData] loaded font_src", font_data.font_src);
-			this._init(image_element);
-		});
+	Module.prototype.drawImage = function (context, x, y) {
+		context.drawImage(this.data, x, y);
+	}
+	
+	Module.prototype.drawImageData = function (context, x, y) {
+		context.putImageData(this.data, x, y);
+	}
+	
+	Module.prototype.drawImageClip = function (context, x, y, clip_x, clip_y, clip_width, clip_height) {
+		//context.drawImage(img,sx,sy,swidth,sheight,x,y,width,height);
+		context.drawImage(this.data, clip_x, clip_y, clip_width, clip_height, x, y, clip_width, clip_height);
+	}
+	
+	Module.prototype.drawImageDataClip = function (context, x, y, clip_x, clip_y, clip_width, clip_height) {
+		//context.putImageData(imgData,x,y,dirtyX,dirtyY,dirtyWidth,dirtyHeight);
+		context.putImageData(this.data, x, y, clip_x, clip_y, clip_width, clip_height);
 	}
 	
 	return Module;
