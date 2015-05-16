c3716356fb6ca558329879b3ae4678488a6f2e2a
ThatBean <Goomailbean@gmail.com>
2015-05-17 00:24:12 +0800

    .

--- files/Dr.Image/image.js
+++ files/Dr.Image/image.js
@@ -70,7 +70,7 @@ Dr.Implement('ImageData', function (global, module_get) {
 	
 	Module.type = {
 		IMAGE_ELEMENT: 'IMAGE_ELEMENT',	//fast, but not editable
-		CANVAS_ELEMENT: 'CANVAS_ELEMENT',	//fast, with vector graph edit API
+		CANVAS_ELEMENT: 'CANVAS_ELEMENT',	//fast, with vector graph edit API(recommend to use)
 		CANVAS_IMAGE_DATA: 'CANVAS_IMAGE_DATA',	//slow, but with pixel manipulation
 	}
 	
@@ -93,7 +93,7 @@ Dr.Implement('ImageData', function (global, module_get) {
 				this.drawClip = this.drawImageDataClip;
 				break;
 			default:
-				Dr.log('[ImageData] error type:', type, source);
+				Dr.log('[ImageData][init] error type:', type, source);
 				break;
 		}
 		
@@ -110,21 +110,92 @@ Dr.Implement('ImageData', function (global, module_get) {
 	}
 	
 	Module.prototype.drawImage = function (context, x, y) {
+		//for IMAGE_ELEMENT, CANVAS_ELEMENT
 		context.drawImage(this._data, x, y);
 	}
 	
 	Module.prototype.drawImageData = function (context, x, y) {
+		//for CANVAS_IMAGE_DATA
 		context.putImageData(this._data, x, y);
 	}
 	
 	Module.prototype.drawImageClip = function (context, x, y, clip_x, clip_y, clip_width, clip_height) {
+		//for IMAGE_ELEMENT, CANVAS_ELEMENT
 		//context.drawImage(img,sx,sy,swidth,sheight,x,y,width,height);
 		context.drawImage(this._data, clip_x, clip_y, clip_width, clip_height, x, y, clip_width, clip_height);
 	}
 	
 	Module.prototype.drawImageDataClip = function (context, x, y, clip_x, clip_y, clip_width, clip_height) {
+		//for CANVAS_IMAGE_DATA
 		//context.putImageData(imgData,x,y,dirtyX,dirtyY,dirtyWidth,dirtyHeight);
 		context.putImageData(this._data, clip_x ? x - clip_x : x, clip_y ? y - clip_y : y, clip_x, clip_y, clip_width, clip_height);
+	}
+	
+	Module.prototype.toCanvas = function () {
+		switch (this.type) {
+			case Module.type.IMAGE_ELEMENT:
+			case Module.type.CANVAS_IMAGE_DATA:
+				var canvas_element = Dr.document.createElement('canvas');
+				canvas_element.width = this.width;
+				canvas_element.height = this.height;
+				this.draw(canvas_element.getContext('2d'), 0, 0);
+				this.init(this._source, canvas_element, Module.type.CANVAS_ELEMENT);
+				break;
+			case Module.type.CANVAS_ELEMENT:
+				return;
+				break;
+			default:
+				Dr.log('[ImageData][toCanvas] error type:', this.type, this._source);
+				break;
+		}
+	}
+	
+	//canvas only
+	Module.prototype.scale = function (scale_x, scale_y) {
+		this.toCanvas();
+		
+		var scale_x = scale_x;
+		var scale_y = scale_y || scale_x;
+		var canvas_element = this.data;
+		
+		Dr.log('scale:', scale_x, scale_y);
+		
+		//get source
+		var source_canvas_image_data = canvas_element.getContext('2d').getImageData(0, 0, canvas_element.width, canvas_element.height);
+		var source_pixel_array = source_canvas_image_data.data;
+		var source_pixel_width = canvas_element.width;
+		Dr.log('source canvas_element size:', canvas_element.width, canvas_element.height);
+		
+		//scale canvas
+		canvas_element.width = this.width * scale_x;
+		canvas_element.height = this.height * scale_y;
+		
+		//get target
+		var target_canvas_image_data = canvas_element.getContext('2d').getImageData(0, 0, canvas_element.width, canvas_element.height);
+		var target_pixel_array = target_canvas_image_data.data;
+		var target_pixel_width = canvas_element.width;
+		Dr.log('source canvas_element size:', canvas_element.width, canvas_element.height);
+		
+		var target_pixel_count = canvas_element.width * canvas_element.height;
+		for (var target_pixel_index = 0; target_pixel_index < target_pixel_count; target_pixel_index++) {
+			var target_x = target_pixel_index % target_pixel_width;
+			var target_y = Math.floor(target_pixel_index / target_pixel_width);
+			
+			var source_x = Math.round(target_x / scale_x);
+			var source_y = Math.round(target_y / scale_y);
+			
+			var target_pixel_array_index = target_pixel_index * 4;
+			var source_pixel_array_index = (source_x + source_y * source_pixel_width) * 4;
+			
+			target_pixel_array[target_pixel_array_index] = source_pixel_array[source_pixel_array_index];
+			target_pixel_array[target_pixel_array_index + 1] = source_pixel_array[source_pixel_array_index + 1];
+			target_pixel_array[target_pixel_array_index + 2] = source_pixel_array[source_pixel_array_index + 2];
+			target_pixel_array[target_pixel_array_index + 3] = source_pixel_array[source_pixel_array_index + 3];
+		}
+		
+		canvas_element.getContext('2d').putImageData(target_canvas_image_data, 0, 0);
+		
+		this.init(this._source, canvas_element, Module.type.CANVAS_ELEMENT);
 	}
 	
 	return Module;
