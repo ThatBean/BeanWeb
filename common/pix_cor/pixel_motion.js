bba3e2ccc7f29474413c0cf7240f2f358e43e825
ThatBean <Goomailbean@gmail.com>
2015-10-26 08:05:05 +0800

    .

--- common/pix_cor/pixel_motion.js
+++ common/pix_cor/pixel_motion.js
@@ -1,17 +1,60 @@
-Dr.Declare('DataHashMap', 'class');
-Dr.Implement('DataHashMap', function (global, module_get) {
+/*
+	A state (key frame) for all PixelBone in a PixelMotion,
+	frames between PixelKeyFrame are auto generated (slop + time)
+*/
+Dr.Declare('PixelKeyFrame', 'class');
+Dr.Require('PixelKeyFrame', 'PixelVector3');
+Dr.Require('PixelKeyFrame', 'PixelRotate3');
+Dr.Implement('PixelKeyFrame', function (global, module_get) {
+	
+	var PixelVector3 = Dr.Get('PixelVector3');	//for position
+	var PixelRotate3 = Dr.Get('PixelRotate3');	//for rotation
 	
 	var Module = function () {
-		this._map = Object.create(null);
+		this.position = new PixelVector3();
+		this.rotation = new PixelVector3();
+		
+		this.pixel_part = null;	//attaching PixelPart
+		
+		this.render_method = null;	//default = null(pixel), can set as block, block-frame a
 	}
 	
-	Module.prototype.checkKey = function (key) {
-		return Object.hasOwnProperty.call(this._map, key);
+	Module.prototype.getId = function () {
+		return this.id;
 	};
 	
-	Module.prototype.getMap = function () {
-		return this._map;
+	return Module;
+});
+
+
+/*
+	Combine PixelBone - PixelPart and PixelKeyFrame data, 
+	receive delta_time and generate corresponding PixelModel(Different Morph for PixelParts)
+	
+	For game purpose, will add some physics related logic to Frame Generation
+*/
+
+Dr.Declare('PixelMotion', 'class');
+Dr.Require('PixelMotion', 'PixelVector3');
+Dr.Require('PixelMotion', 'PixelRotate3');
+Dr.Implement('PixelMotion', function (global, module_get) {
+	
+	var PixelVector3 = Dr.Get('PixelVector3');	//for position
+	var PixelRotate3 = Dr.Get('PixelRotate3');	//for rotation
+	
+	var Module = function () {
+		this.position = new PixelVector3();
+		this.rotation = new PixelVector3();
+		
+		this.pixel_part = null;	//attaching PixelPart
+		
+		this.render_method = null;	//default = null(pixel), can set as block, block-frame a
+	}
+	
+	Module.prototype.getId = function () {
+		return this.id;
 	};
+	
 	
 	return Module;
 });
