c2833d3d596a25b3cc84c6e4a73783da1fadfa6f
ThatBean <Goomailbean@gmail.com>
2015-11-09 08:16:14 +0800

    need invest time...

--- common/pix_cor/pixel_motion.js
+++ common/pix_cor/pixel_motion.js
@@ -26,6 +26,39 @@ Dr.Implement('PixelKeyFrame', function (global, module_get) {
 	return Module;
 });
 
+/*
+	Interpolation of many data type:
+		Vector
+		Number
+		Color
+		Rotation
+	For auto generate key frame between PixelKeyFrame.
+	Interpolation(from_data, to_data, ratio_progress)
+*/
+Dr.Declare('PixelInterpolation', 'class');
+Dr.Require('PixelInterpolation', 'PixelVector3');
+Dr.Require('PixelInterpolation', 'PixelRotate3');
+Dr.Implement('PixelInterpolation', function (global, module_get) {
+	
+	var PixelVector3 = Dr.Get('PixelVector3');	//for position
+	var PixelRotate3 = Dr.Get('PixelRotate3');	//for rotation
+	
+	var Module = function () {
+		this.position = new PixelVector3();
+		this.rotation = new PixelVector3();
+		
+		this.pixel_part = null;	//attaching PixelPart
+		
+		this.render_method = null;	//default = null(pixel), can set as block, block-frame a
+	}
+	
+	Module.prototype.getId = function () {
+		return this.id;
+	};
+	
+	return Module;
+});
+
 
 /*
 	Combine PixelBone - PixelPart and PixelKeyFrame data, 
